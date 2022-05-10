fa376dc8260453703a625a94680439b7e0d12fa5
Pierre van Zyl <pierrevanzyl777@gmail.com>
2022-05-10 23:07:16 +0200

    TrueFalseQuestion class created and extended from Question class

--- src/MultipleChoiceQuestion.java
+++ src/MultipleChoiceQuestion.java
@@ -1,11 +1,6 @@
 import javax.swing.JOptionPane;
 
-public class MultipleChoiceQuestion {
-	static int nQuestions = 0;
-	static int nCorrect = 0;
-	
-	String question;
-	String correctAnswer;
+public class MultipleChoiceQuestion extends Question {
 	
 	public MultipleChoiceQuestion(String query, String a, String b, String c, String d, String e, String answer) {
 		question = query + "\n";
@@ -40,22 +35,7 @@ public class MultipleChoiceQuestion {
 		}
 		return false;
 	}
-	public void check() {
-		nQuestions = nQuestions + 1;
-		String answer = ask();
-		answer = answer.toUpperCase();
-		
-		if (answer.equals(correctAnswer)) {
-			nCorrect = nCorrect + 1;
-			showResults();
-		}else {
-			showResults();
-		}
-		
-		}
-	public static void showResults() {
-		JOptionPane.showMessageDialog(null,nCorrect + " Correct out of " + nQuestions + " questions.");
-	}
+	
 	
 
 }
--- src/Quiz.java
+++ src/Quiz.java
@@ -6,18 +6,20 @@ public class Quiz {
 	
 
 	public static void main(String[] args) {
-		MultipleChoiceQuestion question = new MultipleChoiceQuestion("What is my favorite OS?", "Macintosh","Windows","Linux","Android","IOS","b");
+		/*Question question = new MultipleChoiceQuestion("What is my favorite OS?", "Macintosh","Windows","Linux","Android","IOS","b");
 		question.check();
-		MultipleChoiceQuestion question2 = new MultipleChoiceQuestion("What is my favorite Programming Language?","Ruby","PHP","Java","Python","C#","D");
+		Question question2 = new MultipleChoiceQuestion("What is my favorite Programming Language?","Ruby","PHP","Java","Python","C#","D");
 		question2.check();
-		MultipleChoiceQuestion question3 = new MultipleChoiceQuestion("What is my favorite Web Development Library?","Asp.net","Angular","Vue","Django","React","e");
+		Question question3 = new MultipleChoiceQuestion("What is my favorite Web Development Library?","Asp.net","Angular","Vue","Django","React","e");
 		question3.check();
-		MultipleChoiceQuestion question4 = new MultipleChoiceQuestion("What is my IDE of choice?","Visual Studio","Eclipse","Atom","Notepad++","X-code","A");
+		Question question4 = new MultipleChoiceQuestion("What is my IDE of choice?","Visual Studio","Eclipse","Atom","Notepad++","X-code","A");
 		question4.check();
-		MultipleChoiceQuestion question5 = new MultipleChoiceQuestion("What is my Cloud Storage Provider of choice?","Google Drive","Microsoft Onedrive","Dropbox","iCloud","pCloud","A");
+		Question question5 = new MultipleChoiceQuestion("What is my Cloud Storage Provider of choice?","Google Drive","Microsoft Onedrive","Dropbox","iCloud","pCloud","A");
 		question5.check();
-		MultipleChoiceQuestion question6 = new MultipleChoiceQuestion("How good is Git?","1/5","2/5","3/5","4/5","5/5","E");
-		question6.check();
+		Question question6 = new MultipleChoiceQuestion("How good is Git?","1/5","2/5","3/5","4/5","5/5","E");
+		question6.check();*/
+		Question question7 = new TrueFalseQuestion("Has Donald Trump ever been the President of the United States?", "TRUE");
+		question7.check();
 	}
 
 }
